3911cc541f6833d235d1e62b52d1e4fcee021f21
Yu Kobayashi <yukoba@gmail.com>
2025-03-29 19:27:19 +0900

    Move nes_py/nes to the top folder

--- nes_py/__init__.py
+++
@@ -1,6 +0,0 @@
-"""The nes-py NES emulator for Python 2 & 3."""
-from .nes_env import NESEnv
-
-
-# explicitly define the outward facing API of this package
-__all__ = [NESEnv.__name__]
--- nes_py/wrappers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Wrappers for altering the functionality of the game."""
-from .joypad_space import JoypadSpace
-
-
-# explicitly define the outward facing API of this package
-__all__ = [JoypadSpace.__name__]
